e20c3df93c4807e4fff5fbcbc54d73baadda125b
just1nkorir <justin.korir@student.moringaschool.com>
2023-05-04 12:22:05 +0300

    updates

---
+++ book/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Library from './components/Library';
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Library/>
+    </>
+  );
+}
+
+export default App;
+
